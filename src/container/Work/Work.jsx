ff36c5485191d639a9b8d8c3a2396b893aa49601
martinbogado <martinbogado@live.com.ar>
2022-04-25 15:20:44 -0300

    Work section - mobile card & Footer section - avoid resize on textarea

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -100,11 +100,35 @@ const Work = () => {
             </div>
 
             <div className='app__work-content app__flex'>
+
               <h4 className='bold-text'>{work.title}</h4>
+
               <p className='p-text' style={{ marginTop: 10}}>{work.description}</p>
             
               <div className='app__work-tag app__flex'>
                 <p className='p-text'>{work.tags[0]}</p>
+              </div>
+
+              <div className='app__work-mobile app__flex'>
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ opacity:[0, 1] }}
+                    transition={{duration: 0.2 }}
+                    className='app__flex'
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ opacity:[0, 1] }}
+                    transition={{duration: 0.2 }}
+                    className='app__flex'
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
               </div>
             </div>
           </div>
