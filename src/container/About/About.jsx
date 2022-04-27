2c953a172b0bb2e332f0e2ab9013b6e5c9018995
martinbogado <martinbogado@live.com.ar>
2022-04-27 12:46:12 -0300

    About section finished

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -36,22 +36,30 @@ const About = () => {
         </motion.div>
       </div>
 
-      <div className='app__about-content'>
+      <motion.div 
+        whileInView={{ x: [100,0], opacity: [0, 1] }}
+        transition={{ duration: 0.8 }}
+        className='app__about-content'
+      >
         <h2>About Me</h2>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <button>
+        <motion.p
+          whileInView={{ x: [50,0], opacity: [0, 1] }}
+          transition={{ delay: 0.2 }}
+        >
+        I'm a Graduate Student from <strong>Henry</strong>'s Bootcamp. 
+        I developed my skills as a Full Stack Developer, 
+        but nowadays I seek to improve my work as a <strong>Frontend and Web3 Developer</strong>.
+        During the pandemic, I decided to change the course of my life and I was encouraged to enter the IT world.
+        <br /><br />
+        I love developing innovative and creative projects. I'm always looking for new challenges and I am usually quite 
+        meticulous when it comes to working. I look for a way to make my projects pleasing to the eye.
+        </motion.p>
+        <motion.button whileInView={{ opacity:[0,1]}} transition={{ duration: 0.3 }}>
           <a href={cv} download />
           
           Download CV <AiOutlineCloudDownload/>
-
-        </button>
-      </div>
+        </motion.button>
+      </motion.div>
     </div>
   )
 }
